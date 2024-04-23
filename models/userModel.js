1dd22c66cff70c968e9e964f4ac0e23ba4cd6066
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-04-23 07:57:40 +0000

    feat #3: Add logmodel + update user schema

--- models/userModel.js
+++ models/userModel.js
@@ -1,54 +1,39 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const moment = require('moment');
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
+  _id: {
+    type: mongoose.Schema.Types.ObjectId,
+    auto: true,
+    required: [true, 'A user must have an id'],
+    unique: true,
   },
-  location: {
+  email: {
     type: String,
-    default: ''
+    required: [true, 'Provide your email address'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Provide a valid or unique email'],
   },
   password: {
     type: String,
-    required: true
+    required: [true, 'Provide your password'],
+    minlength: 8,
+    select: false
   },
-  outages: [{
-    date: {
-      type: Date,
-      required: true
-    },
-    timeOff: {
-      type: String,
-      required: true,
-      validate: {
-        validator: function(v) {
-          return moment(v, 'YYYY-MM-DD HH:mm:ss').isAfter(moment().subtract(1, 'minute'));
-        },
-        message: 'Power outage time off must be in the future'
-      }
-    },
-    timeBackOn: {
-      type: String,
-      required: true,
-      validate: {
-        validator: function(v, outage) {
-          const timeOff = moment(outage.timeOff, 'YYYY-MM-DD HH:mm:ss');
-          return moment(v, 'YYYY-MM-DD HH:mm:ss').isAfter(timeOff);
-        },
-        message: 'Power back on time must be after power off time'
-      }
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Confirm your password'],
+    validate: {
+      //Only works on CREATE and SAVE
+      validator: function(el) {
+      return el === this.password;
+      },
+      message: 'Passwords are not the same'
     }
-  }]
-});
-
-userSchema.pre('save', function (next) {
-  next();
+  },
 });
 
 const User = mongoose.model('User', userSchema);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = User;
