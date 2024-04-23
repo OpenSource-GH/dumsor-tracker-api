93effb7a5dc237268f22b7b83e4bd9ab68a70cf0
Leslie <96585664+starboyles@users.noreply.github.com>
2024-04-23 22:36:42 +0000

    Revert "Issue13"

--- models/userModel.js
+++ models/userModel.js
@@ -1,61 +1,54 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const { PhoneNumberUtil } = require('libphonenumber-js');
+const moment = require('moment');
 
 const userSchema = new mongoose.Schema({
-  _id: {
-    type: mongoose.Schema.Types.ObjectId,
-    auto: true,
-    required: [true, 'A user must have an id'],
-    unique: true,
-  },
-  email: {
+  username: {
     type: String,
-    required: [true, 'Provide your email address'],
-    unique: true,
-    lowercase: true,
-    validate: [validator.isEmail, 'Provide a valid or unique email'],
+    required: true,
+    unique: true
+  },
+  location: {
+    type: String,
+    default: ''
   },
   password: {
     type: String,
-    required: [true, 'Provide your password'],
-    minlength: 8,
-    select: false,
+    required: true
   },
-  passwordConfirm: {
-    type: String,
-    required: [true, 'Confirm your password'],
-    validate: {
-      // Only works on CREATE and SAVE
-      validator: function(el) {
-        return el === this.password;
-      },
-      message: 'Passwords are not the same',
+  outages: [{
+    date: {
+      type: Date,
+      required: true
     },
-  },
-  phoneNumber: {
-    type: String,
-    required: true,
-    minlength: 10,
-    validate: {
-      validator: function(value) {
-        try {
-          const phoneUtil = PhoneNumberUtil.getInstance();
-          const phoneNumber = phoneUtil.parseAndKeepRawInput(value, 'GH');
-          return phoneUtil.isValidNumber(phoneNumber);
-        } catch (error) {
-          return false;
-        }
-      },
-      message: 'Invalid phone number format',
+    timeOff: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function(v) {
+          return moment(v, 'YYYY-MM-DD HH:mm:ss').isAfter(moment().subtract(1, 'minute'));
+        },
+        message: 'Power outage time off must be in the future'
+      }
     },
-  },
+    timeBackOn: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function(v, outage) {
+          const timeOff = moment(outage.timeOff, 'YYYY-MM-DD HH:mm:ss');
+          return moment(v, 'YYYY-MM-DD HH:mm:ss').isAfter(timeOff);
+        },
+        message: 'Power back on time must be after power off time'
+      }
+    }
+  }]
 });
 
-userSchema.pre('save', function(next) {
+userSchema.pre('save', function (next) {
   next();
 });
 
 const User = mongoose.model('User', userSchema);
 
-module.exports = User;
+module.exports = mongoose.model('User', userSchema);
--- readme.md
+++ readme.md
@@ -1,22 +1,14 @@
-## Dumsor Tracker API
+# Dumsor Tracker API
 
-### Overview
-The Dumsor Tracker API project aims to monitor and analyze power outage patterns across the country.
+### About
+We're building this project to track the pattern of power outages across the country.
 
-### Technology Stack
-- Next.js with TypeScript
-- Node.js with Express
+### Stack
+- NextJS + TypeScript
+- Node + Express
 
-
-### Application Workflow
-- Authentication: The application will require authentication. Users can view data without creating an account, but will have to create one to post data.
-
-- Reporting Power Outages: Users can report power outages by visiting the application and providing the following details:
-  - Location
-  - Power Status: Whether the user has electricity or not.
-
-This streamlined approach will allow for easy data submission and help in tracking and analyzing power outage trends effectively.
-
-## Contributing
-Please read our [Contribution Guidelines](contributing/CONTRIBUTING.md) before contributing to the project.
-
+### App Flow
+- Authentication: There will be no authentication. Users can provide data without having an account.
+- When your power it cut off, simply visit the app, and provide the following details:
+    - Your location
+    - Power Status: Do you have light or not?
