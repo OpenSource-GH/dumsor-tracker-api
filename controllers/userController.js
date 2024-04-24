b4b3803c28619fdfa543c4922e40a1018fe496d9
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-04-24 09:04:20 +0000

    feat: Implement getAllUsers  endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,23 @@
 const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
 
-exports.createUser = (req, res) => {
-    res.status(500).json({
-      status: 'error',
-      message: 'this route is not defined yet',
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      status: 'Success',
+      data: {
+        results: users.length,
+        users,
+      },
     });
-  };
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+});
 
 exports.createUser = (req, res) => {
   res.status(500).json({
