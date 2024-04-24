bf739cf7b4a4772438ff46f5b43cf1ab15191f40
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-04-24 22:20:20 +0000

    Remove unnecessary AppError

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,6 @@ const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
-const AppError = require('./../utils/appError.js');
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
