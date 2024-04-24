c7c193ddf9487d53c67c18e17b935186219fe0e4
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-04-24 23:47:30 +0000

    Updated authController for Supabase authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,28 +2,24 @@
 const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 //const catchAsync = require('./../utils/catchAsync');
-
 const jwt = require('jsonwebtoken');
 const supabase = require('../supabase'); 
 
+
 exports.signup = async (req, res, next) => {
   const { email, password } = req.body;
-
   try {
     const { user, session, error } = await supabase.auth.signUp({
       email,
       password,
     });
-
     if (error) {
       return next(new AppError(error.message, 400));
     }
-
     // Generate JWT token for user (optional)
     const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
       expiresIn: process.env.JWT_EXPIRES_IN,
     });
-
     res.status(201).json({
       status: 'success',
       data: {
