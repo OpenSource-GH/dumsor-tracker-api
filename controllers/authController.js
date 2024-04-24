9a0e340aa8d6df03fdfef909f99a38384a12d095
Leslie <96585664+starboyles@users.noreply.github.com>
2024-04-24 23:18:14 +0000

    Update authController.js for supabase auth integration

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,66 +1,3 @@
-
-const { promisify } = require("util");
-const jwt = require("jsonwebtoken");
-const User = require("./../models/userModel");
-const catchAsync = require("./../utils/catchAsync");
-const AppError = require("./../utils/appError.js");
-
-
-exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm,
-    phoneNumber: req.body.phoneNumber,
-  });
-
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
-
-  res.status(201).json({
-    status: "success",
-    data: {
-      token,
-      user: newUser,
-    },
-  });
-});
-
-exports.login = (req, res, next) => {
-  const { email, password } = req.body;
-
-  //check if email and password do exist
-  if (!email || !password) {
-    res.status(404).json({
-      status: "fail",
-      message: "Provide email and password",
-    });
-  } else {
-    User.find({ email: req.body.email })
-      .exec()
-      .then((user) => {
-        if (user.length < 1) {
-          return res.status(401).json({
-            message: "User does not exist",
-          });
-        }
-        //send token to client if everything is on point
-        const token = "";
-        res.status(201).json({
-          status: "success",
-          token,
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(404).json({
-          status: "fail",
-          message: "An error occured whiles loggin in",
-        });
-      });
-=======
 //const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
@@ -99,7 +36,6 @@ exports.signup = async (req, res, next) => {
   }
 };
 
-// Login user with Supabase
 exports.login = async (req, res, next) => {
   const { email, password } = req.body;
 
@@ -112,7 +48,6 @@ exports.login = async (req, res, next) => {
     if (error) {
       return next(new AppError(error.message, 401));
     }
-
     // Generate JWT token for user (optional)
     const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
       expiresIn: process.env.JWT_EXPIRES_IN,
@@ -127,7 +62,6 @@ exports.login = async (req, res, next) => {
     });
   } catch (err) {
     return next(new AppError('Failed to log in user', 500));
-
   }
 };
 
