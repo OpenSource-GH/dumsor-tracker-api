cef4737d0d50d90540d7d948d74e690369c9275c
Daniel-Sogbey <mathematics06physics@gmail.com>
2024-04-24 12:02:12 +0000

    added pagination logic to getting all users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,36 @@
-const User = require('./../models/userModel');
-const catchAsync = require('./../utils/catchAsync');
+const User = require("./../models/userModel");
+const catchAsync = require("./../utils/catchAsync");
 
+//Added pagination logic to the getAllUsers controller
 exports.getAllUsers = catchAsync(async (req, res, next) => {
+  // Set default values for page and limit
+  const page = parseInt(req.query.page, 10) || 1;
+  const pageSize = parseInt(req.query.pageSize, 10) || 10;
+
+  // Calculate the skip based on the page and pageSize
+  const skip = (page - 1) * pageSize;
+
   try {
-    const users = await User.find();
+    // Find all users with pagination
+    const users = await User.find().skip(skip).limit(pageSize);
+
+    // Calculate the total number of users
+    const totalUsers = await User.countDocuments();
+
+    // Send response with pagination information
     res.status(200).json({
-      status: 'Success',
+      status: "Success",
       data: {
         results: users.length,
         users,
+        page,
+        pageSize,
+        totalUsers,
       },
     });
   } catch (err) {
     res.status(404).json({
-      status: 'fail',
+      status: "fail",
       message: err.message,
     });
   }
@@ -21,25 +38,25 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
 
 exports.createUser = (req, res) => {
   res.status(500).json({
-    status: 'error',
-    message: 'this route is not defined yet',
+    status: "error",
+    message: "this route is not defined yet",
   });
 };
 exports.getUser = (req, res) => {
   res.status(500).json({
-    status: 'error',
-    message: 'this route is not defined yet',
+    status: "error",
+    message: "this route is not defined yet",
   });
 };
 exports.updateUser = (req, res) => {
   res.status(500).json({
-    status: 'error',
-    message: 'this route is not defined yet',
+    status: "error",
+    message: "this route is not defined yet",
   });
 };
 exports.deleteUser = (req, res) => {
   res.status(500).json({
-    status: 'error',
-    message: 'this route is not defined yet',
+    status: "error",
+    message: "this route is not defined yet",
   });
 };
