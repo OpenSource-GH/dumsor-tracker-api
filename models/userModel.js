814dd952169229ec5e5b468b4bbdb2153e49169d
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-04-28 20:38:25 +0000

    fix: libphonenumber-js

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const moment = require('moment');
 const { PhoneNumberUtil } = require('libphonenumber-js');
 
 const userSchema = new mongoose.Schema({
